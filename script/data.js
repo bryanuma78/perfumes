f00f5a5b4c9f58265915bc563a4079f6299e9498
jwolf <bradjoey7@gmail.com>
2025-09-02 11:40:27 -0500

    Actualizacion

---
+++ script/data.js
@@ -0,0 +1,615 @@
+const descripciones = {
+  armaf: {
+    titulo: "ARMAF ODYSSEY MANDARIN SKY 3.4 EDP VARON",
+    img: "imagenes/armaf-odyssey.jpg",
+    precio: 65,
+    genero: "hombre",
+    descripcion: {
+      familia: "Cítrica aromática fresca",
+      salida: "Mandarina, limón, bergamota, notas verdes",
+      corazon: "Lavanda, salvia, geranio",
+      fondo: "Almizcle blanco, ámbar gris, madera de cedro"
+    }
+  },
+  fakhar: {
+    titulo: "FAKHAR WOMEN 3.4. OZ. EDP",
+    img: "imagenes/fakhar.jpg",
+    precio: 50,
+    genero: "mujer",
+    descripcion: {
+      familia: "Floral oriental",
+      salida: "Frutas rojas, bergamota, naranja",
+      corazon: "Rosa, jazmín, lirio del valle",
+      fondo: "Vainilla, almizcle, pachulí"
+    }
+  },
+  museRose: {
+    titulo: "Muse Rose Musc (Pour Femme) - 100ML by Mirada",
+    img: "imagenes/muse-rose.jpg",
+    precio: 40,
+    genero: "mujer",
+    descripcion: {
+      familia: "Floral almizclada",
+      salida: "Rosa, peonía",
+      corazon: "Muguet, flores blancas",
+      fondo: "Almizcle, ámbar"
+    }
+  },
+  loveForever: {
+    titulo: "LOVE FOREVER PINK FASCINÓ EAU DE PARFUM 80ml",
+    img: "imagenes/love-forever.jpg",
+    precio: 40,
+    genero: "mujer",
+    descripcion: {
+      familia: "Floral frutal",
+      salida: "Frutas tropicales, frambuesa",
+      corazon: "Rosa, jazmín",
+      fondo: "Vainilla, almizcle"
+    }
+  },
+  tribalGold: {
+    titulo: "TRIBAL GOLD GRANDEUR ELITE by Grandeur",
+    img: "imagenes/tribal-gold.jpg",
+    precio: 50,
+    genero: "hombre",
+    descripcion: {
+      familia: "Oriental especiada",
+      salida: "Bergamota, cardamomo",
+      corazon: "Canela, clavo, jazmín",
+      fondo: "Ámbar, madera de cedro, vainilla"
+    }
+  },
+  museNoire: {
+    titulo: "Muse Beauté Noire (Pour Femme) - 100ML Edp by Mirada",
+    img: "imagenes/muse-noire.jpg",
+    precio: 40,
+    genero: "mujer",
+    descripcion: {
+      familia: "Floral oriental",
+      salida: "Grosella negra, pera",
+      corazon: "Jazmín, iris, rosa",
+      fondo: "Vainilla, haba tonka, praliné"
+    }
+  },
+  fraisPink: {
+    titulo: "Frais et Frais PINK PARADISE",
+    img: "imagenes/frais-pink.jpg",
+    precio: 60,
+    genero: "mujer",
+    descripcion: {
+      familia: "Frutal floral",
+      salida: "Mango, maracuyá, mandarina",
+      corazon: "Peonía, rosa, jazmín",
+      fondo: "Almizcle blanco, sándalo"
+    }
+  },
+  alwataniah: {
+    titulo: "ALWATANIAH- GRANADA 75ML",
+    img: "imagenes/alwataniah.jpg",
+    precio: 45,
+    genero: "mujer",
+    descripcion: {
+      familia: "Oriental frutal",
+      salida: "Granada, frutos rojos",
+      corazon: "Rosa, jazmín",
+      fondo: "Pachulí, almizcle, ámbar"
+    }
+  },
+  candyWhite: {
+    titulo: "CANDY WHITE EAU DE PARFUM 100ml By Grandeur",
+    img: "imagenes/candy-white.jpg",
+    precio: 45,
+    genero: "mujer",
+    descripcion: {
+      familia: "Gourmand floral",
+      salida: "Caramelo, pera, cítricos",
+      corazon: "Jazmín, flor de azahar",
+      fondo: "Vainilla, almizcle, sándalo"
+    }
+  },
+  fraisOpulente: {
+    titulo: "Frais et Frais Opulente 3.4 oz",
+    img: "imagenes/frais-opulente.jpg",
+    precio: 60,
+    genero: "mujer",
+    descripcion: {
+      familia: "Floral oriental",
+      salida: "Mandarina, pimienta rosa",
+      corazon: "Rosa, violeta, lirio",
+      fondo: "Ámbar, pachulí, almizcle"
+    }
+  },
+  fraisFlorale: {
+    titulo: "Frais et Frais Florale 3.4oz",
+    img: "imagenes/frais-florale.png",
+    precio: 60,
+    genero: "mujer",
+    descripcion: {
+      familia: "Floral fresca",
+      salida: "Lima, manzana verde",
+      corazon: "Peonía, lirio, jazmín",
+      fondo: "Almizcle blanco, cedro"
+    }
+  },
+  laVivacite: {
+    titulo: "LA VIVACITE by MAISON ALHAMBRA women",
+    img: "imagenes/la-vivacite.png",
+    precio: 50,
+    genero: "mujer",
+    descripcion: {
+      familia: "Floral gourmand",
+      salida: "Grosella negra, pera",
+      corazon: "Iris, jazmín, flor de azahar",
+      fondo: "Praliné, vainilla, haba tonka"
+    }
+  },
+  laVieFemme: {
+    titulo: "LA VIE FEMME by Grandeur",
+    img: "imagenes/la-vie-femme.jpg",
+    precio: 50,
+    genero: "mujer",
+    descripcion: {
+      familia: "Floral gourmand",
+      salida: "Grosella negra, pera",
+      corazon: "Iris, jazmín, flor de azahar",
+      fondo: "Praliné, vainilla, haba tonka"
+    }
+  },
+  redCherry: {
+    titulo: "RED CHERRY CÁMARA WOMEN – 100ML By Camara",
+    img: "imagenes/red-cherry.jpg",
+    precio: 50,
+    genero: "mujer",
+    descripcion: {
+      familia: "Frutal floral",
+      salida: "Cereza, frambuesa",
+      corazon: "Rosa, jazmín",
+      fondo: "Almizcle, ámbar"
+    }
+  },
+  dresscode: {
+    titulo: "Dresscode (Pour Femme) - 85ML by Mirada",
+    img: "imagenes/dresscode.jpg",
+    precio: 45,
+    genero: "mujer",
+    descripcion: {
+      familia: "Floral elegante",
+      salida: "Bergamota, melocotón",
+      corazon: "Jazmín, rosa",
+      fondo: "Sándalo, almizcle"
+    }
+  },
+  dresscodeHer: {
+    titulo: "Dresscode For Her 100 ml by Mirada",
+    img: "imagenes/dresscode-her.jpg",
+    precio: 45,
+    genero: "mujer",
+    descripcion: {
+      familia: "Floral frutal",
+      salida: "Mandarina, frambuesa",
+      corazon: "Peonía, rosa, jazmín",
+      fondo: "Vainilla, almizcle blanco"
+    }
+  },
+  bodyMistBlack: {
+    titulo: "Body Mist Black Opus 250ml by Camara",
+    img: "imagenes/body-mist-black.jpg",
+    precio: 10,
+    genero: "mujer",
+    descripcion: {
+      familia: "Aromática fresca",
+      salida: "Notas cítricas, frutos ligeros",
+      corazon: "Flores blancas, lavanda",
+      fondo: "Almizcle suave, ámbar"
+    }
+  },
+  bodySplashMango: {
+    titulo: "BODY SPLASH MANGO TEMPTATION COOL & COOL 160ml",
+    img: "imagenes/body-splash-mango.jpg",
+    precio: 10,
+    genero: "mujer",
+    descripcion: {
+      familia: "Frutal floral",
+      salida: "Mango, notas cítricas",
+      corazon: "Flores blancas",
+      fondo: "Almizcle ligero"
+    }
+  },
+
+    setDiva: {
+      titulo: "SET DIVA SWISS COLLECTION by Diva",
+      img: "imagenes/set-diva.jpg",
+      precio: 50,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral frutal",
+        salida: "Frutas dulces",
+        corazon: "Rosa, jazmín",
+        fondo: "Almizcle"
+      }
+    },
+    clubNuitMen: {
+      titulo: "CLUB DE NUIT INTENSE MEN 105 ml by Armaf",
+      img: "imagenes/club-nuit-men.jpg",
+      precio: 70,
+      genero: "hombre",
+      descripcion: {
+        familia: "Amaderada especiada",
+        salida: "Cítricos, pimienta negra",
+        corazon: "Rosa, jazmín",
+        fondo: "Ámbar, vainilla, madera"
+      }
+    },
+    clubMaleka: {
+      titulo: "CLUB DE NUIT MALEKA EAU DE PARFUM by Armaf",
+      img: "imagenes/club-maleka.jpg",
+      precio: 90,
+      genero: "mujer",
+      descripcion: {
+        familia: "Oriental amaderada",
+        salida: "Azafrán, pimienta rosa",
+        corazon: "Rosa, jazmín",
+        fondo: "Ámbar, pachulí, vainilla"
+      }
+    },
+    alNujoom: {
+      titulo: "AL NUJOOM MOON women by Zakat",
+      img: "imagenes/al-nujoom.jpg",
+      precio: 45,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral oriental",
+        salida: "Cítricos, grosella negra",
+        corazon: "Rosa, violeta, jazmín",
+        fondo: "Almizcle, madera, ámbar"
+      }
+    },
+    coolLotion: {
+      titulo: "Cool & Cool Body Lotion Silky Comfort – 500 ml",
+      img: "imagenes/cool-lotion.jpg",
+      precio: 18,
+      genero: "mujer",
+      descripcion: {
+        familia: "Aromática ligera",
+        salida: "Notas frescas de cítricos",
+        corazon: "Flores suaves",
+        fondo: "Almizcle, crema hidratante"
+      }
+    },
+    delilah: {
+      titulo: "DELILAH ALL OVER MAISON ALHAMBRA 150ml",
+      img: "imagenes/delilah.jpg",
+      precio: 30,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral almizclada",
+        salida: "Pera, bergamota",
+        corazon: "Rosa, lirio",
+        fondo: "Almizcle, ámbar"
+      }
+    },
+    perfumeChants: {
+      titulo: "PERFUME CHANTS ALL OVER by MAISON ALHAMBRA 150ml",
+      img: "imagenes/perfume-chants.jpg",
+      precio: 25,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral fresca",
+        salida: "Notas cítricas",
+        corazon: "Jazmín, peonía",
+        fondo: "Almizcle blanco"
+      }
+    },
+    mayarCherry: {
+      titulo: "MAYAR CHERRY INTENSE by LATTAFA women",
+      img: "imagenes/mayar-cherry.jpg",
+      precio: 70,
+      genero: "mujer",
+      descripcion: {
+        familia: "Frutal gourmand",
+        salida: "Cereza, frutos rojos",
+        corazon: "Rosa, jazmín",
+        fondo: "Vainilla, almizcle"
+      }
+    },
+    perfumeVivacite: {
+      titulo: "PERFUME ALL OVER LA VIVACITE MAISON ALHAMBRA 150ml",
+      img: "imagenes/perfume-vivacite.jpg",
+      precio: 25,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral elegante",
+        salida: "Mandarina, pera",
+        corazon: "Peonía, jazmín",
+        fondo: "Almizcle, ámbar"
+      }
+    },
+    myYara: {
+      titulo: "My Yara Collection 5ml 4pcs Gift Set by Lattafa",
+      img: "imagenes/my-yara.jpg",
+      precio: 70,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral frutal variada",
+        salida: "Notas frutales",
+        corazon: "Flores variadas",
+        fondo: "Almizcle, ámbar, vainilla"
+      }
+    },
+    laCharmante: {
+      titulo: "LA CHARMANTE ECLANTANT MAISON ALHAMBRA 100ml",
+      img: "imagenes/la-charmante.jpg",
+      precio: 50,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral sofisticada",
+        salida: "Notas cítricas",
+        corazon: "Rosa, jazmín",
+        fondo: "Almizcle, madera"
+      }
+    },
+    extremeGentleman: {
+      titulo: "Extreme Gentleman (Pour Homme) - 100ML by Mirada",
+      img: "imagenes/extreme-gentleman.jpg",
+      precio: 45,
+      genero: "hombre",
+      descripcion: {
+        familia: "Amaderada especiada",
+        salida: "Bergamota, pimienta",
+        corazon: "Lavanda, nuez moscada",
+        fondo: "Cedro, pachulí, ámbar"
+      }
+    },
+    fraisLuxe: {
+      titulo: "Frais et Frais LUXE DORE to woman",
+      img: "imagenes/frais-luxe.jpg",
+      precio: 60,
+      genero: "mujer",
+      descripcion: {
+        familia: "Oriental floral",
+        salida: "Naranja, especias",
+        corazon: "Jazmín, rosa",
+        fondo: "Vainilla, almizcle, ámbar"
+      }
+    },
+    luminous: {
+      titulo: "LUMINOUS VIVID by MAISON ALHAMBRA 100ml",
+      img: "imagenes/luminous-vivid.jpg",
+      precio: 45,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral fresca",
+        salida: "Notas cítricas",
+        corazon: "Flores blancas",
+        fondo: "Almizcle, sándalo"
+      }
+    },
+    leFalcone: {
+      titulo: "By LE FALCONE NICHE- MIRSAAL PASSION 100ML",
+      img: "imagenes/le-falcone.jpg",
+      precio: 60,
+      genero: "unisex",
+      descripcion: {
+        familia: "Oriental amaderada",
+        salida: "Cítricos, especias",
+        corazon: "Rosa, jazmín",
+        fondo: "Pachulí, ámbar, madera"
+      }
+    },
+    lattafaAjayed: {
+      titulo: "LATTAFA AJAYED DUBAI PORTRAIT EDP 100ml",
+      img: "imagenes/lattafa-ajayed.jpg",
+      precio: 50,
+      genero: "unisex",
+      descripcion: {
+        familia: "Amaderada oriental",
+        salida: "Cítricos, especias",
+        corazon: "Flores orientales",
+        fondo: "Ámbar, madera, vainilla"
+      }
+    },
+    alAraaf: {
+      titulo: "AL A’ RAAF RAMAL by Ramal",
+      img: "imagenes/al-araaf.jpg",
+      precio: 50,
+      genero: "unisex",
+      descripcion: {
+        familia: "Oriental especiada",
+        salida: "Pimienta rosa, cardamomo",
+        corazon: "Rosa, jazmín",
+        fondo: "Ámbar, oud, almizcle"
+      }
+    },
+    perlaSara: {
+      titulo: "Perla Sara (Pour Femme) – 100ML – by Mirada",
+      img: "imagenes/perla-sara.jpg",
+      precio: 45,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral frutal",
+        salida: "Mandarina, melocotón",
+        corazon: "Peonía, rosa, jazmín",
+        fondo: "Almizcle, sándalo"
+      }
+    },
+    mayarNatural: {
+      titulo: "Mayar Natural Intense Eau de Parfum Spray for Women by Lattafa",
+      img: "imagenes/mayar-natural.jpg",
+      precio: 70,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral frutal",
+        salida: "Frutas tropicales, cítricos",
+        corazon: "Rosa, jazmín, lirio",
+        fondo: "Almizcle blanco, vainilla"
+      }
+    },
+    mayarPink: {
+      titulo: "Mayar Pink Eau de Parfum Spray for Women by Lattafa",
+      img: "imagenes/mayar-pink.jpg",
+      precio: 70,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral dulce",
+        salida: "Frambuesa, fresa",
+        corazon: "Jazmín, rosa",
+        fondo: "Vainilla, almizcle"
+      }
+    },
+    yaraTous: {
+      titulo: "Yara Tous (pineapple scence) Eau de Parfum for Women by Lattafa",
+      img: "imagenes/yara-tous.jpg",
+      precio: 50,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral frutal",
+        salida: "Mandarina, coco",
+        corazon: "Peonía, jazmín",
+        fondo: "Almizcle, vainilla"
+      }
+    },
+    nebrasPride: {
+      titulo: "Nebras Pride (exclusive women perfume) by Lattafa",
+      img: "imagenes/nebras-pride.jpg",
+      precio: 70,
+      genero: "mujer",
+      descripcion: {
+        familia: "Oriental gourmand",
+        salida: "Caramelo, frutas dulces",
+        corazon: "Rosa, jazmín",
+        fondo: "Vainilla, almizcle, ámbar"
+      }
+    },
+    zeroPlus: {
+      titulo: "Zero Plus Feminine (Eau de Toilette) by Diesel",
+      img: "imagenes/zero-plus.jpg",
+      precio: 28,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral oriental",
+        salida: "Bergamota, ciruela, canela",
+        corazon: "Jazmín, rosa, lirio",
+        fondo: "Vainilla, almizcle, sándalo"
+      }
+    },
+    perlaPassion: {
+      titulo: "Perla Passion 100 ml by Mirada",
+      img: "imagenes/perla-passion.jpg",
+      precio: 45,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral frutal",
+        salida: "Mandarina, pera, frutos rojos",
+        corazon: "Rosa, jazmín, peonía",
+        fondo: "Almizcle blanco, sándalo, vainilla"
+      }
+    },
+    delilahMaison: {
+      titulo: "Delilah by Maison Alhambra",
+      img: "imagenes/delilah-maison.jpg",
+      precio: 30,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral ambarada",
+        salida: "Bergamota, grosella negra",
+        corazon: "Rosa turca, peonía, jazmín",
+        fondo: "Vainilla, almizcle, madera de cachemira"
+      }
+    },
+    pureMaracuyea: {
+      titulo: "Pure Maracuyea limited edition by Zakat",
+      img: "imagenes/pure-maracuyea.jpg",
+      precio: 40,
+      genero: "mujer",
+      descripcion: {
+        familia: "Frutal tropical",
+        salida: "Maracuyá, piña, cítricos",
+        corazon: "Mango, flor de azahar, jazmín",
+        fondo: "Almizcle, ámbar ligero"
+      }
+    },
+    eclair: {
+      titulo: "Éclair Eau de Parfum for Women by Lattafa",
+      img: "imagenes/eclair.jpg",
+      precio: 60,
+      genero: "mujer",
+      descripcion: {
+        familia: "Gourmand floral",
+        salida: "Caramelo, avellana, frutas rojas",
+        corazon: "Rosa, jazmín",
+        fondo: "Vainilla, almizcle, sándalo"
+      }
+    },
+    domeCasabella: {
+      titulo: "Dome Casabella by Camara",
+      img: "imagenes/dome-casabella.jpg",
+      precio: 45,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral oriental",
+        salida: "Cítricos, pera",
+        corazon: "Jazmín, rosa, lirio",
+        fondo: "Vainilla, almizcle, ámbar"
+      }
+    },
+    peachBlast: {
+      titulo: "Peach Blast by Camara",
+      img: "imagenes/peach-blast.jpg",
+      precio: 50,
+      genero: "mujer",
+      descripcion: {
+        familia: "Frutal gourmand",
+        salida: "Durazno, mandarina",
+        corazon: "Jazmín, flor de azahar",
+        fondo: "Vainilla, almizcle"
+      }
+    },
+    badeeOud: {
+      titulo: "BADEE AL OUD AMETHYST by women 100 ml",
+      img: "imagenes/badee-oud.jpeg",
+      precio: 50,
+      genero: "mujer",
+      descripcion: {
+        familia: "Amaderada oriental",
+        salida: "Especias, cítricos",
+        corazon: "Rosa, jazmín",
+        fondo: "Oud, ámbar, almizcle"
+      }
+    },
+    fraisSupreme: {
+      titulo: "Frais et Frais Supreme (exclusive essence) by women",
+      img: "imagenes/frais-supreme.jpeg",
+      precio: 70,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral oriental",
+        salida: "Cítricos, frutas dulces",
+        corazon: "Jazmín, rosa",
+        fondo: "Ámbar, almizcle, vainilla"
+      }
+    },
+    armafCandee: {
+      titulo: "ARMAF ODYSSEY CANDEE to women",
+      img: "imagenes/armaf-candee.jpeg",
+      precio: 60,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral frutal",
+        salida: "Frutas tropicales, manzana",
+        corazon: "Rosa, jazmín",
+        fondo: "Almizcle blanco, vainilla"
+      }
+    },
+    glacierBella: {
+      titulo: "GLACIER BELLA WOMEN 100ML by MAISON ALHAMBRA",
+      img: "imagenes/glacier-bella.jpeg",
+      precio: 50,
+      genero: "mujer",
+      descripcion: {
+        familia: "Floral gourmand",
+        salida: "Mandarina, pera",
+        corazon: "Rosa, jazmín",
+        fondo: "Vainilla, almizcle"
+      }
+    }
+ };
---
+++ script/script.js
@@ -0,0 +1,185 @@
+const perfumeContainer = document.getElementById("perfumeContainer");
+const carritoCount = document.getElementById("carritoCount");
+const carritoLista = document.getElementById("carritoLista");
+
+// Inicializar carrito
+let carrito = [];
+
+function renderPerfumes() {
+  perfumeContainer.innerHTML = "";
+  for (let key in descripciones) {
+    const perfume = descripciones[key];
+    perfumeContainer.innerHTML += `
+      <div class="col">
+        <div class="card h-100 shadow-sm">
+          <img src="${perfume.img}" class="card-img-top" alt="${perfume.titulo}">
+          <div class="card-body text-center">
+            <h5 class="card-title">${perfume.titulo}</h5>
+            <p class="card-text"><strong>Familia:</strong> ${perfume.descripcion.familia}</p>
+            <p class="card-text text-info"><strong>Género:</strong> ${perfume.genero}</p>  <!-- AÑADIDO -->
+            <p class="card-text text-success"><strong>Precio:</strong> $${perfume.precio.toFixed(2)}</p>
+            
+            <div class="input-group mb-2 justify-content-center" style="max-width: 120px; margin: auto;">
+              <span class="input-group-text">Cant.</span>
+              <input type="number" min="0" value="0" class="form-control cantidadInput" id="cantidad-${key}">
+            </div>
+
+            <button class="btn btn-dark mt-2" onclick="agregarAlCarrito('${key}')">Agregar al carrito</button>
+            <button class="btn btn-outline-warning mt-2" onclick="verDescripcion('${key}')">Ver descripción</button>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
+
+
+// Mostrar descripción en modal
+function verDescripcion(key) {
+  const perfume = descripciones[key];
+  document.getElementById("descripcionTitulo").textContent = perfume.titulo;
+  document.getElementById("descripcionImg").src = perfume.img;
+  document.getElementById("descripcionImg").alt = perfume.titulo;
+  document.getElementById("descFamilia").textContent = perfume.descripcion.familia;
+  document.getElementById("descSalida").textContent = perfume.descripcion.salida;
+  document.getElementById("descCorazon").textContent = perfume.descripcion.corazon;
+  document.getElementById("descFondo").textContent = perfume.descripcion.fondo;
+  document.getElementById("descPrecio").textContent = `$${perfume.precio.toFixed(2)}`;
+  document.getElementById("descGenero").textContent = perfume.genero; // <-- Agregado
+
+  const modal = new bootstrap.Modal(document.getElementById("modalDescripcion"));
+  modal.show();
+}
+
+
+// Agregar al carrito con cantidad
+function agregarAlCarrito(key) {
+  const cantidadInput = document.getElementById(`cantidad-${key}`);
+  const cantidad = parseInt(cantidadInput.value);
+
+  if (!cantidad || cantidad <= 0) {
+    mostrarToast("Agrega una cantidad válida antes de agregar al carrito.");
+    return;
+  }
+
+  // Buscar si el producto ya está en el carrito
+  const indexExistente = carrito.findIndex(item => item.key === key);
+  if (indexExistente !== -1) {
+    carrito[indexExistente].cantidad += cantidad;
+  } else {
+    carrito.push({
+      key: key,
+      ...descripciones[key],
+      cantidad: cantidad
+    });
+  }
+
+  carritoCount.textContent = carrito.reduce((acc, item) => acc + item.cantidad, 0);
+  renderCarrito();
+  cantidadInput.value = 0; // Reiniciar input
+}
+
+// Renderizar carrito
+function renderCarrito() {
+  if (carrito.length === 0) {
+    carritoLista.innerHTML = `<p class="text-muted">Tu carrito está vacío.</p>`;
+    return;
+  }
+
+  let totalProductos = 0;
+  let totalPrecio = 0;
+
+  carritoLista.innerHTML = `
+    <ul class="list-group mb-3">
+      ${carrito.map((item, i) => {
+        totalProductos += item.cantidad;
+        totalPrecio += item.precio * item.cantidad;
+        return `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          <div>
+            <strong>${item.titulo}</strong><br>
+            <small class="text-muted">${item.descripcion.familia}</small><br>
+            <small class="text-success">Precio: $${item.precio.toFixed(2)} x ${item.cantidad} = $${(item.precio * item.cantidad).toFixed(2)}</small>
+          </div>
+          <button class="btn btn-sm btn-danger" onclick="eliminarDelCarrito(${i})">🗑</button>
+        </li>
+      `}).join("")}
+    </ul>
+    <p><strong>Total de productos:</strong> ${totalProductos}</p>
+    <p><strong>Total a pagar:</strong> $${totalPrecio.toFixed(2)}</p>
+  `;
+}
+
+// Eliminar del carrito
+function eliminarDelCarrito(index) {
+  carrito.splice(index, 1);
+  carritoCount.textContent = carrito.reduce((acc, item) => acc + item.cantidad, 0);
+  renderCarrito();
+}
+
+// -----------------------------
+// FUNCION FINALIZAR COMPRA (WhatsApp)
+// -----------------------------
+function finalizarCompra() {
+  if (carrito.length === 0) {
+    mostrarToast("El carrito está vacío.");
+    return;
+  }
+
+  let mensaje = "🛍️ *Nuevo Pedido:*\n\n";
+  let totalProductos = 0;
+  let totalPrecio = 0;
+
+  carrito.forEach(item => {
+    mensaje += `${item.titulo} x${item.cantidad} - $${(item.precio * item.cantidad).toFixed(2)}\n`;
+    totalProductos += item.cantidad;
+    totalPrecio += item.precio * item.cantidad;
+  });
+
+  mensaje += `\n📦 Total de productos: ${totalProductos}`;
+  mensaje += `\n💰 Total a pagar: $${totalPrecio.toFixed(2)}`;
+  mensaje += "\n✅ Por favor confirme el pedido.";
+
+  let numeroVendedora = "593992570322";
+  let url = `https://wa.me/${numeroVendedora}?text=${encodeURIComponent(mensaje)}`;
+  window.open(url, "_blank");
+}
+
+// -----------------------------
+// TOAST
+// -----------------------------
+function mostrarToast(mensaje) {
+  let toastContainer = document.getElementById("toastContainer");
+  if (!toastContainer) {
+    toastContainer = document.createElement("div");
+    toastContainer.id = "toastContainer";
+    toastContainer.className = "position-fixed bottom-0 end-0 p-3";
+    toastContainer.style.zIndex = 1060;
+    document.body.appendChild(toastContainer);
+  }
+
+  const toastEl = document.createElement("div");
+  toastEl.className = "toast align-items-center text-bg-warning border-0";
+  toastEl.role = "alert";
+  toastEl.ariaLive = "assertive";
+  toastEl.ariaAtomic = "true";
+  toastEl.innerHTML = `
+    <div class="d-flex">
+      <div class="toast-body">
+        ${mensaje}
+      </div>
+      <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"></button>
+    </div>
+  `;
+
+  toastContainer.appendChild(toastEl);
+  const toast = new bootstrap.Toast(toastEl, { delay: 3000 });
+  toast.show();
+
+  toastEl.addEventListener("hidden.bs.toast", () => {
+    toastEl.remove();
+  });
+}
+
+// Inicializar
+renderPerfumes();
